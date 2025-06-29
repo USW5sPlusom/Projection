340f409497e69553f475d4e3aa491db0c11b8637
Alexey <vaznoe2024@mail.ru>
2025-06-29 23:41:20 +0300

    v.3

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
 @main.route("/")
 def home():
-    return "Начало"
+    return render_template('index.html')
+
+@main.route("/algorithms")
+def algorithms():
+    return render_template("algorithms.html")
+
+@main.route("/data-structures")
+def data_structures():
+    return render_template("data_structures.html")
